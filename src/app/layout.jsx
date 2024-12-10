23138abc96cbaed140f746fa2f82aea3cdcf275d
aidqen <idanmarkin8@gmail.com>
2024-12-10 11:02:20 +0200

    Quick Start

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import { Inter } from '@next/font/google';
+
+export const metadata = {
+  title: "planwise",
+  description: "Enhance your productivity with AI integrated to Google Calendar!",
+};
+
+const inter = Inter({
+  subsets: ['latin'], // Loads characters for most Western languages
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // Includes all weights
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body
+        className={`font-[inter]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
