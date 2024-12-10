1d418ef256071bcb64db70871a34340d265bb148
aidqen <idanmarkin8@gmail.com>
2024-12-10 16:50:13 +0200

    Fetch user and try to fetch Calendar (fail)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,24 +1,21 @@
-import "./globals.css";
-import { Inter } from '@next/font/google';
+import { SessionProvider } from 'next-auth/react'
+import './globals.css'
+import { Inter } from '@next/font/google'
 
 export const metadata = {
-  title: "planwise",
-  description: "Enhance your productivity with AI integrated to Google Calendar!",
-};
+  title: 'planwise',
+  description: 'Enhance your productivity with AI integrated to Google Calendar!',
+}
 
 const inter = Inter({
   subsets: ['latin'], // Loads characters for most Western languages
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // Includes all weights
-});
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
-      <body
-        className={`font-[inter]`}
-      >
-        {children}
-      </body>
+      <body className={`font-[inter]`}>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,29 @@
+'use client'
+import { getUserSession } from "@/lib/session";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetchUser()
+    fetchCalendar()
+  }, [])
+  
+  async function fetchUser() {
+    const user = await getUserSession()
+    setUser(user)
+  }
+
+  async function fetchCalendar() {
+    const response = await fetch("/api/calendar-events");
+    const data = await response.json()
+    console.log('data:', data)
+  }
   return (
     <div className="flex flex-col items-start justify-start min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
+      {JSON.stringify(user)}
     </div>
   );
 }
