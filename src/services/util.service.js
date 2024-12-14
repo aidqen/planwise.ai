d2ff88a777322306c8958088c8e7fdd4b4c79bc7
aidqen <idanmarkin8@gmail.com>
2024-12-15 00:19:38 +0200

    Add makeId function

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,11 +1,20 @@
 export function hexToRgba(hex, opacity) {
 
-    hex = hex.replace('#', '');
-  
-    const r = parseInt(hex.substring(0, 2), 16);
-    const g = parseInt(hex.substring(2, 4), 16);
-    const b = parseInt(hex.substring(4, 6), 16);
-  
-    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  hex = hex.replace('#', '');
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+export function makeId(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  
+  return result;
+}
