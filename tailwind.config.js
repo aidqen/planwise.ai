150520963ee22abe0a410156791503d7d3c76e44
aidqen <idanmarkin8@gmail.com>
2024-12-14 15:45:26 +0200

    Add shadcn setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        mainLight: "#fafbfc",
-        secondaryLight: '#FFFFFF',
-        textColorLight: '#212226',
-        foreground: "var(--foreground)",
-      },
-      // I want to make font size variables in tailwind
-      fontSize: {
-        xs: "0.75rem",
-        sm: "0.875rem",
-        base: "1rem",
-        lg: "1.125rem",
-        xl: "1.25rem",
-        "2xl": "1.5rem",
-      },
-    },
+  	extend: {
+  		colors: {
+  			mainLight: '#fafbfc',
+  			secondaryLight: '#FFFFFF',
+  			textColorLight: '#212226',
+  			foreground: 'var(--foreground)'
+  		},
+  		fontSize: {
+  			xs: '0.75rem',
+  			sm: '0.875rem',
+  			base: '1rem',
+  			lg: '1.125rem',
+  			xl: '1.25rem',
+  			'2xl': '1.5rem'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
