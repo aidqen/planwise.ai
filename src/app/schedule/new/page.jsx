7d8f33ec8ddc2e76f6344b0341df569a5ba75ffb
aidqen <idanmarkin8@gmail.com>
2024-12-15 23:58:44 +0200

    Remove step 4 Review, add Confetti and new route

--- src/app/schedule/new/page.jsx
+++ src/app/schedule/new/page.jsx
@@ -5,63 +5,74 @@ import { useEffect, useState } from 'react'
 import { Preferences } from './components/Preferences'
 import { Breadcrumbs } from '@/components/Breadcrumbs'
 import { AddGoals } from './components/AddGoals'
+import { Routines } from './components/Routines'
+import { ConfettiButton } from '@/components/ui/confetti-button'
 
 export default function ScheduleNew() {
   const [step, setStep] = useState(0)
   const router = useRouter()
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams()
   const totalSteps = 3
 
   useEffect(() => {
-    const currentSearchParams = new URLSearchParams(searchParams.toString());
-    currentSearchParams.set('step', step.toString()); // Add/update 'step' param
-    router.push(`/schedule/new?${currentSearchParams.toString()}`, { shallow: true });
-  }, [step, searchParams, router]);
+    const currentSearchParams = new URLSearchParams(searchParams.toString())
+    currentSearchParams.set('step', step.toString()) // Add/update 'step' param
+    router.push(`/schedule/new?${currentSearchParams.toString()}`, { shallow: true })
+  }, [step, searchParams, router])
 
   function goBackStep() {
-    setStep((prev) => Math.max(prev - 1, 0));
-  };
+    setStep(prev => Math.max(prev - 1, 0))
+  }
 
   function goForwardStep() {
-    setStep((prev) => prev + 1);
-  };
+    setStep(prev => prev + 1)
+  }
 
   function renderStepContent() {
     switch (step) {
       case 0:
-        return <Preferences />;
+        return <Preferences />
       case 1:
-        return <AddGoals />;
+        return <AddGoals />
       case 2:
-        return <Routines />;
-      case 3:
-        return <Reviews />;
+        return <Routines />
       default:
-        return <div>Step not found</div>;
+        router.push('/schedule/new/final')
     }
   }
 
   return (
-    <div className="relative flex flex-col items-start justify-start mt-5 w-full h-[calc(100%-5em)] text-black pt-10 rounded-xl ">
-      <Breadcrumbs currentIdx={step} setStep={setStep}/>
-      <div className="h-[1px] w-full bg-black/10"></div>
-      {renderStepContent()}
-      <div className="absolute bottom-0 left-0 w-full flex flex-row items-center justify-between py-10">
+    <div className="relative flex flex-col items-center justify-between max-sm:mt-5 mt-2 max-sm:w-full w-[40%]  h-[calc(100%-5em)] text-black pt-10 rounded-xl ">
+      <div className="w-full max-sm:block flex flex-col items-center">
+        <Breadcrumbs currentIdx={step} setStep={setStep} />
+        <div className="h-[1px] w-full bg-black/10"></div>
+        {renderStepContent()}
+      </div>
+      <div className=" max-sm:w-full w-[20em] flex flex-row items-center justify-between pt-7">
         <button
           onClick={goBackStep}
-          className=
-          "px-10 py-2.5 rounded-lg font-medium transition-all duration-300 transform hover:scale-[1.02] bg-white hover:bg-gray-50 text-gray-700 shadow-md border border-gray-200"
+          className="px-10 py-2.5 rounded-lg font-medium transition-all duration-300 transform hover:scale-[1.02] bg-white hover:bg-gray-50 text-gray-700 shadow-md border border-gray-200"
         >
           Back
         </button>
-        <button
-          onClick={goForwardStep}
-          className=
-          "px-8 py-2.5 rounded-lg font-medium transition-all duration-300 transform hover:scale-[1.02] bg-green-400 hover:bg-green-500 text-white shadow-md border border-green-300 whitespace-nowrap"
-        >
-          Save and Next
-        </button>
+        {step === 2 ? (
+          <ConfettiButton>
+            <button
+              onClick={goForwardStep}
+              className="px-8 py-2.5 rounded-lg font-medium transition-all duration-300 transform hover:scale-[1.02] bg-secondary hover:bg-secondary/80 text-white shadow-md border border-secondary whitespace-nowrap"
+            >
+              Complete
+            </button>
+          </ConfettiButton>
+        ) : (
+          <button
+            onClick={goForwardStep}
+            className="px-8 py-2.5 rounded-lg font-medium transition-all duration-300 transform hover:scale-[1.02] bg-green-400 hover:bg-green-500 text-white shadow-md border border-green-300 whitespace-nowrap"
+          >
+            Save and Next
+          </button>
+        )}
       </div>
-    </div >
+    </div>
   )
 }
