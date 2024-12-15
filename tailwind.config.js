5c03452696fef187716ca2c3fbac358e51e7d40e
aidqen <idanmarkin8@gmail.com>
2024-12-15 23:58:03 +0200

    Better define my color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			mainLight: '#fafbfc',
-  			secondaryLight: '#FFFFFF',
-  			textColorLight: '#212226',
-  			foreground: 'var(--foreground)'
-  		},
-  		fontSize: {
-  			xs: '0.75rem',
-  			sm: '0.875rem',
-  			base: '1rem',
-  			lg: '1.125rem',
-  			xl: '1.25rem',
-  			'2xl': '1.5rem'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        mainLight: '#fafbfc',
+        secondaryLight: '#FFFFFF',
+        primary: 'rgb(74 222 128)',
+        secondary: 'rgb(59, 130, 246)',
+        neutral: 'rgb(156, 163, 175)',
+        textColorLight: '#212226',
+        foreground: 'var(--foreground)',
+      },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
-};
+  plugins: [require('tailwindcss-animate')],
+}
