730a78d46b2622983b236d3076c9f028af9336b2
aidqen <idanmarkin8@gmail.com>
2024-12-15 23:59:04 +0200

    Finish preferences functionality and styling

--- src/app/schedule/new/components/Preferences.jsx
+++ src/app/schedule/new/components/Preferences.jsx
@@ -1,21 +1,49 @@
-import { ScheduleIntensity } from "@/components/ScheduleIntensity";
-import TimePicker from "@/components/TimePicker";
+'use client'
+import { ScheduleIntensity } from '@/components/ScheduleIntensity'
+import TimePicker from '@/components/TimePicker'
+import { Section } from '@/components/ui/section'
+import { useState } from 'react'
 
-export function Preferences({ }) {
+export function Preferences({}) {
+  const [preferences, setPreferences] = useState({
+    wakeup: '7:00 AM',
+    sleep: '10:00 PM',
+    intensity: 'relaxed',
+  })
+  console.log('preferences:', preferences)
 
-    const timeTypes = ["Wake Up", "Sleep"];
-    return <>
-        {/* <h1 className='text-2xl text-green-600'>Preferences</h1> */}
-        <div className="flex flex-col gap-7 w-full mt-7">
+  const timeTypes = ['Wake Up', 'Sleep']
 
-            <div className="flex flex-row justify-between items-center w-full mb-3">
-                {timeTypes.map(timeType => <div key={timeType} className="flex flex-col items-start justify-start gap-2">
-                    <label className='font-normal text-lg text-black/80'>{timeType}</label>
-                    <TimePicker timeType={timeType} />
-                </div>)}
+  function handleIntensityChange(target) {
+    console.log('target:', target)
+    setPreferences(state => ({ ...state, intensity: target }))
+  }
+
+  return (
+    <Section>
+      <div className="flex flex-col max-sm:items-start items-center gap-5 w-full mt-7">
+        <h1 className="text-xl mb-2">Add your preferences!</h1>
+        <div className="flex flex-row justify-between items-center max-sm:w-full w-[30em] mb-3">
+          {timeTypes.map(timeType => (
+            <div
+              key={timeType}
+              className="flex flex-col items-start justify-start gap-2"
+            >
+              <label className="font-normal text-base text-black/80">{timeType}</label>
+              <TimePicker
+                timeType={timeType}
+                wakeup={preferences?.wakeup}
+                sleep={preferences?.sleep}
+                setPreferences={setPreferences}
+              />
             </div>
-            <ScheduleIntensity />
+          ))}
         </div>
-
-    </>
+        <ScheduleIntensity
+          handleChange={handleIntensityChange}
+          intensity={preferences?.intensity}
+        />
+      </div>
+    </Section>
+  )
 }
