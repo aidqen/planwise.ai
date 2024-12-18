bdfe58cbfc1460c4a53b721d0620cea085376538
aidqen <idanmarkin8@gmail.com>
2024-12-18 23:35:52 +0200

    handle test request from OpenAI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,32 @@
 import { useSelector } from 'react-redux'
 import { HomepageCard } from './components/HomepageCard'
 import { HomepageActions } from './components/HomepageActions'
+import OpenAI from "openai";
+import { useEffect } from 'react';
 
 export default function Home() {
   const user = useSelector(state => state.userModule.user)
+
+  useEffect(() => {
+    handleGetRequest()
+  }, [])
+  
+
+  const handleGetRequest = async () => {
+    try {
+      const res = await fetch('/api/test-gpt', { method: 'POST' });
+      const data = await res.json();
+
+      if (res.ok) {
+        console.log('GET Response:', data);
+      } else {
+        console.error('GET Error:', data.error);
+      }
+    } catch (error) {
+      console.error('GET Request Failed:', error);
+    }
+  };
+
 
   console.log('user:', user)
   return (
@@ -13,7 +36,7 @@ export default function Home() {
         <p className="mt-12 xl:mt-5 mb-4 text-[#B6BBC3] max-sm:text-base text-xl w-max capitalize">
           Hello {user?.name?.split(' ').slice(0, 1)} 👋
         </p>
-        <h1 className="xl:text-5xl text-4xl text-black/80 text-center font-bold mb-14">
+        <h1 className="xl:text-5xl max-sm:text-3xl text-black/80 text-center font-bold mb-14">
           Own your day.
           <br /> Plan with purpose, achieve with{' '}
           <span className="text-green-500 underline">ease.</span>
