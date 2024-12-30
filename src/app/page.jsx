654290602652e0afd53c650dbedec1a394573151
aidqen <idanmarkin8@gmail.com>
2024-12-30 22:39:45 +0200

    Edit Homepage styling, not finished

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,47 +8,12 @@ import { useEffect } from 'react';
 export default function Home() {
   const user = useSelector(state => state.userModule.user)
 
-  // useEffect(() => {
-  //   handleGetRequest()
-  // }, [])
-  
-
-  // const handleGetRequest = async () => {
-  //   try {
-  //     const res = await fetch('/api/test-gpt', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify({
-  //         preferences: { wakeTime: '07:00', sleepTime: '22:00', intensity: 'moderate' },
-  //         routines: [
-  //           { id: 1, title: 'Work', start: '9:00', end: '17:00' },
-  //         ],
-  //         goals: [
-  //           { id: 1, title: 'Learn about indie hacking' },
-  //           { id: 2, title: 'Look better' },
-  //           { id: 3, title: 'Keep in touch with family or friends' },
-  //         ],
-  //       }),
-  //     });
-  
-  //     const data = await res.json();
-  
-  //     if (res.ok) {
-  //       console.log('Response:', data);
-  //     } else {
-  //       console.error('Error:', data.error);
-  //     }
-  //   } catch (error) {
-  //     console.error('Request Failed:', error);
-  //   }
-  // };
 
 
-  console.log('user:', user)
   return (
 
-      <div className="flex overflow-y-auto flex-col justify-start items-center pb-10 w-full h-full sm:p-20">
-        <div className="flex flex-col items-center w-[50%]">
+    <div className="flex overflow-y-auto flex-col justify-start items-center pb-10 w-full h-full sm:p-20">
+      <div className="flex flex-col items-center w-[60%]">
 
         <p className="flex mt-12 xl:mt-5 mb-4 text-[#B6BBC3] max-sm:text-base text-xl w-max capitalize">
           Hello {user?.name?.split(' ').slice(0, 1)} 👋
@@ -58,9 +23,13 @@ export default function Home() {
           <br /> Plan with purpose, achieve with{' '}
           <span className="text-green-500 underline">ease.</span>
         </h1>
-        <HomepageCard />
-        </div>
-        <HomepageActions />
+        {/* <HomepageCard /> */}
+        <button className="px-6 py-2 mt-4 text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+          Get Started
+        </button>
+
+        {/* <HomepageActions /> */}
       </div>
+    </div>
   )
 }
