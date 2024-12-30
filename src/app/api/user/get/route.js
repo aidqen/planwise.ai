b21615c3c132f9e7351a4a7fb8c74f01c1107570
aidqen <idanmarkin8@gmail.com>
2024-12-30 22:42:16 +0200

    get user by email route

--- src/app/api/user/get/route.js
+++ src/app/api/user/get/route.js
@@ -1,16 +1,23 @@
 import { getUserSession } from '@/lib/session'
 import { authOptions } from '../../auth/[...nextauth]/route'
+import { NextResponse } from 'next/server';
+import { dbService } from '../../services/db.service';
 
 
 export async function GET(req) {
   try {
-    const userDetails = getUserSession(authOptions)
-    // const user = await prisma.user.findFirst({where: {_id: userDetails.id}}) // Modify query as needed
-    return Response.json(user)
-  } catch (error) {
-    console.error('Error fetching user:', error)
-    return Response.json({ error: 'Failed to fetch user' }, { status: 500 })
-  } finally {
-    // await prisma.$disconnect()
+    const { searchParams } = new URL(req.url);
+    const email = searchParams.get('email');
+    console.log("🚀 ~ file: route.js:11 ~ email:", email)
+
+    const usersCollection = await dbService.getCollection('User');
+
+    const user = await usersCollection.findOne({ email });
+    console.log("🚀 ~ file: route.js:11 ~ user:", user)
+
+    return NextResponse.json(user)
+  } catch (err) {
+    console.error('Error in upsert operation:', err);
+    throw err;
   }
 }
