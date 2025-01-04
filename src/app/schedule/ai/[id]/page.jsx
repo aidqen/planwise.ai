8b1087122a735f28e5f5f259888df3b2cdac175f
aidqen <idanmarkin8@gmail.com>
2025-01-04 17:16:53 +0200

    add handleTaskSave and newTask as well

--- src/app/schedule/ai/[id]/page.jsx
+++ src/app/schedule/ai/[id]/page.jsx
@@ -1,30 +1,18 @@
 'use client'
 
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from '@/components/ui/card'
-// import { useState } from 'react'
 import { useSelector } from 'react-redux'
-// import { TaskCard } from './components/TaskCard'
-// import { TaskDialog } from './components/TaskDialog'
 import { useEffect, useState } from 'react'
 
-// import { AddScheduleDialog } from './components/AddScheduleDialog'
-// import { SaveToCalendarBtn } from './components/SaveToCalendarBtn'
-// import { ScheduleStructure } from './components/ScheduleStructure'
 import { useParams } from 'next/navigation'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { ScheduleStructure } from '../components/ScheduleStructure'
-import { TaskCard } from '../components/TaskCard'
 import { SaveToCalendarBtn } from '../components/SaveToCalendarBtn'
 import { AddScheduleDialog } from '../components/AddScheduleDialog'
 import { TaskDialog } from '../components/TaskDialog'
 import { scheduleService } from '@/services/scheduleService'
 import { TaskList } from '../components/TaskList'
+import { Button } from '@/components/ui/button'
+import { format } from 'date-fns'
 
 export default function DailySchedule({ }) {
   const [calendarDialogOpen, setCalendarDialogOpen] = useState(false)
@@ -33,162 +21,14 @@ export default function DailySchedule({ }) {
   const [isVisible, setIsVisible] = useState(true)
   const [lastMouseMove, setLastMouseMove] = useState(Date.now())
   const params = useParams()
-  const [schedule, setSchedule] = useState(null)
-  const multiStepForm = useSelector(state => state.scheduleModule.multiStepForm);
-  const wakeupTime = multiStepForm?.preferences?.wakeup || '04:00';
-  // console.log("🚀 ~ file: page.jsx:35 ~ schedule:", schedule)
+  const [schedule, setSchedule] = useState({schedule: []})
+  console.log("🚀 ~ file: page.jsx:37 ~ schedule:", schedule)
+  const wakeupTime = schedule.preferences?.wakeup || '04:00';
 
   useEffect(() => {
     console.log('params:', params);
     onFetchSchedule()
-  }, [params])
-
-
-
-
-  // const schedule = useSelector(state => state.scheduleModule.schedule);
-  // const schedule = {
-  //   _id: '677871af4e0c76d49095eb9b',
-  //   name: 'Daily Schedule',
-  //   schedule: [
-  //     {
-  //       id: 't1',
-  //       summary: 'Morning Routine',
-  //       description: 'Hygiene and getting dressed for the day',
-  //       start: '07:00',
-  //       end: '07:50',
-  //       category: 'routine'
-  //     },
-  //     {
-  //       id: 't2',
-  //       summary: 'Breakfast',
-  //       description: 'Enjoy a nutritious breakfast',
-  //       start: '07:30',
-  //       end: '08:00',
-  //       category: 'meal'
-  //     },
-  //     {
-  //       id: 't3',
-  //       summary: 'Work on Project A',
-  //       description: 'Focus on key deliverables for Project A',
-  //       start: '08:00',
-  //       end: '09:30',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 'break1',
-  //       summary: 'Short Break',
-  //       description: 'Take a short break to recharge',
-  //       start: '09:30',
-  //       end: '09:45',
-  //       category: 'break'
-  //     },
-  //     {
-  //       id: 't4',
-  //       summary: 'Email and Administrative Tasks',
-  //       description: 'Check emails and complete any admin tasks',
-  //       start: '09:45',
-  //       end: '10:30',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 't5',
-  //       summary: 'Continue Project A',
-  //       description: 'Continue working on Project A tasks',
-  //       start: '10:30',
-  //       end: '12:00',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 't6',
-  //       summary: 'Lunch',
-  //       description: 'Enjoy a healthy lunch break',
-  //       start: '12:00',
-  //       end: '13:00',
-  //       category: 'meal'
-  //     },
-  //     {
-  //       id: 't7',
-  //       summary: 'Team Meeting',
-  //       description: 'Weekly team meeting to discuss progress and plans',
-  //       start: '13:00',
-  //       end: '14:00',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 'break2',
-  //       summary: 'Relaxation Time',
-  //       description: '30 minutes of relaxing to clear your mind',
-  //       start: '14:00',
-  //       end: '14:30',
-  //       category: 'break'
-  //     },
-  //     {
-  //       id: 't8',
-  //       summary: 'Work on Project B',
-  //       description: 'Focus on the tasks for Project B',
-  //       start: '14:30',
-  //       end: '16:00',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 'break3',
-  //       summary: 'Outdoor Walk',
-  //       description: 'Take a walk outside to refresh',
-  //       start: '16:00',
-  //       end: '16:30',
-  //       category: 'break'
-  //     },
-  //     {
-  //       id: 't9',
-  //       summary: 'Project Review',
-  //       description: 'Review completed project tasks and prepare for tomorrow',
-  //       start: '16:30',
-  //       end: '18:00',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 't10',
-  //       summary: 'Dinner',
-  //       description: 'Have a relaxing dinner',
-  //       start: '18:00',
-  //       end: '19:00',
-  //       category: 'meal'
-  //     },
-  //     {
-  //       id: 't11',
-  //       summary: 'Reading',
-  //       description: 'Read a book for personal growth',
-  //       start: '19:00',
-  //       end: '20:00',
-  //       category: 'goal'
-  //     },
-  //     {
-  //       id: 'break4',
-  //       summary: 'Evening Relaxation',
-  //       description: 'Relax and unwind with some leisure activities',
-  //       start: '20:00',
-  //       end: '21:00',
-  //       category: 'break'
-  //     },
-  //     {
-  //       id: 't12',
-  //       summary: 'Night Routine',
-  //       description: 'Prepare for bed and wind down for sleep',
-  //       start: '21:00',
-  //       end: '22:00',
-  //       category: 'routine'
-  //     }
-  //   ],
-  //   createdAt: 1735946671221,
-  //   updatedAt: 1735946671221,
-  //   preferences: { wakeup: '7:00', sleep: '10:00 PM', intensity: 'moderate' },
-  //   routines: [],
-  //   goals: []
-  // }
-
-  // Default to 4:00 AM if wakeup time is not set
-  // const wakeupTime = '07:00';
+  }, [])
 
   useEffect(() => {
     let timeoutId
@@ -230,7 +70,15 @@ export default function DailySchedule({ }) {
     setSelectedTask(null); // Close the modal by clearing the selected task
   }
 
+  function deleteTask(taskId) {
+    const tasksToSave = schedule?.schedule?.filter(prevTask => taskId !== prevTask.id)
+    const scheduleToSave = { ...schedule, schedule: tasksToSave }
+    setSchedule(scheduleToSave)
+  }
+
   async function handleSaveTask(task) {
+    console.log('update task');
+    
     try {
       const tasksToSave = schedule?.schedule?.map(prevTask => task.id === prevTask.id ? task : prevTask)
       const scheduleToSave = { ...schedule, schedule: tasksToSave }
@@ -238,10 +86,18 @@ export default function DailySchedule({ }) {
       // await scheduleService.updateSchedule(schedule)
     } catch (err){
       console.error(err);
-      
     }
   }
 
+  function handleNewTaskSave(task) {
+    console.log('new task');
+    
+    const tasksToSave = [...(schedule?.schedule || []), task];
+    const scheduleToSave = { ...schedule, schedule: tasksToSave };
+    
+    setSchedule(scheduleToSave);
+  }
+  
   async function onFetchSchedule() {
 
     console.log("🚀 ~ file: page.jsx:187 ~ params?.id:", params?.id)
@@ -256,25 +112,26 @@ export default function DailySchedule({ }) {
   const sortedTasks = [...schedule?.schedule]?.sort((a, b) => a.start?.localeCompare(b.start));
 
   return (
-    <Card className="overflow-y-auto mx-auto w-full max-w-5xl bg-transparent border-none ps-9">
+    <Card className="overflow-y-auto mx-auto w-full max-w-5xl bg-transparent border-none scrollbar ps-9">
       <CardHeader className="flex flex-col gap-0 items-center pt-0 pb-5 ps-6 pe-10">
-        <CardTitle className="text-lg font-semibold text-center">Daily Schedule</CardTitle>
+        <CardTitle className="text-lg font-semibold text-center">{schedule?.name}</CardTitle>
         <CardDescription className="mt-0 text-base text-start max-sm:text-sm text-black/60">
-          Here&apos;s your AI generated schedule for today.
+          Last Updated: {format(new Date(schedule?.updatedAt), "MMM d, h:mm a")}
         </CardDescription>
-        <div className="flex justify-end w-full">
-          <button onClick={onCreateTask} className='px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg'>+ Add New Task</button>
+        <div className="flex gap-3 justify-end w-full">
+          <Button onClick={onCreateTask} className='px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg'>+ Add New Task</Button>
+          <Button variant="outline" className="hover:bg-white/70">Save</Button>
         </div>
       </CardHeader>
       <CardContent>
-        <div className="relative h-[1640px] border-l-2 border-gray-300">
+        <div className="relative h-[1640px] w-[calc(100%-2em)] border-l-2 border-gray-300">
           <ScheduleStructure wakeupMinutes={wakeupMinutes} />
           <TaskList tasks={sortedTasks} wakeupMinutes={wakeupMinutes} handleTaskClick={handleTaskClick} />
         </div>
       </CardContent>
       <SaveToCalendarBtn toggleCalendarDialog={toggleCalendarDialog} isVisible={isVisible} />
       <AddScheduleDialog open={calendarDialogOpen} setOpen={setCalendarDialogOpen} schedule={schedule} />
-      <TaskDialog isCreateTask={isCreateTask} selectedTask={selectedTask} handleCloseModal={handleCloseModal} handleSaveTask={handleSaveTask}/>
+      <TaskDialog isCreateTask={isCreateTask} selectedTask={selectedTask} handleCloseModal={handleCloseModal} handleSaveTask={handleSaveTask} handleNewTaskSave={handleNewTaskSave} deleteTask={deleteTask}/>
     </Card>
   );
 }
